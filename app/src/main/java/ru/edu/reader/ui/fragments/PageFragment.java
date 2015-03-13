c4d12655a068ea652279c6fb0f8cd06a59739cf5
Grinch <artem@zappylab.com>
2015-03-14 00:03:43 +0300

    BOOOOOOK

--- app/src/main/java/ru/edu/reader/ui/fragments/PageFragment.java
+++ app/src/main/java/ru/edu/reader/ui/fragments/PageFragment.java
@@ -70,6 +70,7 @@ public class PageFragment extends Fragment {
         return layout;
     }
 
+
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
